41a6003b5cef77f3d53347644b76995130441c32
calevano <calevano@gmail.com>
2014-10-12 22:45:12 -0500

    actualizar info

--- application/models/export_model.php
+++ application/models/export_model.php
@@ -1,13 +1,15 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-	class Export_model extends CI_MODEL
-	{
-		
-		function only_query( $query )
-		{
-			$result = $this->db->query( $query );
-			return $result;
-		}
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-	}
+class Export_model extends CI_MODEL {
+
+    function only_query($query) {
+        $result = $this->db->query($query);
+        return $result;
+    }
+
+}
+
 ?>
