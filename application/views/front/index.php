a6b5bfbaf5530de31cba5d5f3a42e300d2ed3d48
calevano <calevano@gmail.com>
2014-10-16 15:33:36 -0500

    update index.

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -12,7 +12,7 @@
 <!-- Cuerpo -->
 <div id="cuerpo" >
     <div id="msg" class="text-center"></div>
-    
+
     <div class="row ">
         <div class="filtro_map">
             <div class="row">
@@ -27,7 +27,7 @@
                             </div>
                         </div>
                         <!-- Fin Buscador codigo --> 
-                        
+
                         <!-- Buscador Colegio -->
                         <div id="dv_searchColegio" class="form-group col-xs-12 topSearchColegio"  style="margin-bottom: 0;padding-top: 8px;">
                             <label class="preguntas_sub2" for="searchColegio">Nombre de la Institución Educativa </label>
@@ -42,7 +42,6 @@
                             <label class="preguntas_sub2" for="depa">departamento</label>
                             <select id="depa" class="col-xs-12 sinpadding select2" name="depa">
                                 <option value="">Seleccione</option>
-<!--                                <option value="-1">TODOS</option>-->
                                 <!-- cargando departamentos -->
                             </select>
                         </div>
@@ -73,7 +72,7 @@
                         <!-- botones de envio -->
                         <div id="boton_accion" class="form-group col-xs-12 text-center clase_boton_accion">
                             <div class="col-xs-6">
-                                <button name="sendSearch" id="filtrar" class="btn btn-success" onclick="filtrarTablaLista('<?php echo base_url();?>','buscarDatosLocal',1)" type="button" >Buscar</button>
+                                <button name="sendSearch" id="filtrar" class="btn btn-success" onclick="filtrarTablaLista('<?php echo base_url(); ?>', 'buscarDatosLocal', 1)" type="button" >Buscar</button>
                             </div>
                             <div class="col-xs-6">
                                 <button id="limpiar_inputs" class="btn btn-danger " name="sendSearch" >Limpiar</button>
@@ -91,35 +90,49 @@
 </div>
 <!-- Fin Cuerpo -->
 <script type="text/javascript">
-    $("#searchColegio").on("click",function (){
-        $(this).select();
-        $('#prov').empty();
-        $("#prov").append('<option value="">Seleccione</option>');
-        $("#dv_prov .select2-chosen").text("Seleccione");
-        $("#dv_dist .select2-chosen").text("Seleccione");
-        $('#depa option:selected').val("");
-        $("#dv_dep .select2-chosen").text("Seleccione");
-    });
-    
-    $("#searchCodigo").on("click",function (){
-        $(this).select();
-        $('#prov').empty();
-        $("#prov").append('<option value="">Seleccione</option>');
-        $("#dv_prov .select2-chosen").text("Seleccione");
-        $("#dv_dist .select2-chosen").text("Seleccione");
-        $('#depa option:selected').val("");
-        $("#dv_dep .select2-chosen").text("Seleccione");
-    });
-    
-    $("#limpiar_inputs").on("click", function() {
-        $("#searchColegio").val("");
-        $("#searchCodigo").val("");
-        $('#prov').empty();
-        $("#prov").append('<option value="">Seleccione</option>');
-        $("#dv_prov .select2-chosen").text("Seleccione");
-        $("#dv_dist .select2-chosen").text("Seleccione");
-        $('#depa option:selected').val("");
-        $("#dv_dep .select2-chosen").text("Seleccione");
+    $(function() {
         
+
+        $("#searchColegio").on("click", function() {
+            $(this).select();
+            $('#prov').empty();
+            $('#dist').empty();
+            $("#prov").append('<option value="">Seleccione</option>');
+            $("#dist").append('<option value="">Seleccione</option>');
+            $("#dv_dep .select2-chosen").text("Seleccione");
+            $('#depa').prop('selectedIndex',0);
+            $("#depa").val("");
+            $("#dv_prov .select2-chosen").text("Seleccione");
+            $("#dv_dist .select2-chosen").text("Seleccione");
+        });
+
+        $("#searchCodigo").on("click", function() {
+            $(this).select();
+            $('#prov').empty();
+            $('#dist').empty();
+            $("#prov").append('<option value="">Seleccione</option>');
+            $("#dist").append('<option value="">Seleccione</option>');
+            $("#dv_dep .select2-chosen").text("Seleccione");
+            $('#depa').prop('selectedIndex',0);
+            $("#depa").val("");
+            $("#dv_prov .select2-chosen").text("Seleccione");
+            $("#dv_dist .select2-chosen").text("Seleccione");
+        });
+
+        $("#limpiar_inputs").on("click", function() {
+            $("#searchColegio").val("");
+            $("#searchCodigo").val("");
+            $('#prov').empty();
+            $('#dist').empty();
+            $("#prov").append('<option value="">Seleccione</option>');
+            $("#dist").append('<option value="">Seleccione</option>');
+            $("#dv_dep .select2-chosen").text("Seleccione");
+            $('#depa').prop('selectedIndex',0);
+            $("#depa").val("");
+            $("#dv_prov .select2-chosen").text("Seleccione");
+            $("#dv_dist .select2-chosen").text("Seleccione");
+        });
     });
+
+
 </script>
