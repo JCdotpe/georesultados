12c31beae7ea3e256413cb56cc14215d5a2c0f22
calevano <calevano@gmail.com>
2014-10-21 17:58:31 -0500

    create excel the list directory

--- application/views/front/list2.php
+++ application/views/front/list2.php
@@ -1,69 +1,76 @@
-<?php
+<form action="<?php echo base_url()?>ficheroExcel.php" method="post" target="_blank" id="FormularioExportacion">
+    <p>Exportar a Excel  <img src="<?php echo base_url()?>assets/img/icono/export_to_excel.gif" class="botonExcel" /></p>
+    <input type="hidden" id="datos_a_enviar" name="datos_a_enviar" />
+</form>
 
-$dato = "C:/CarlosLevano/01";
-leer_archivos_y_directorios($dato);
+<table  class="table table-striped table-hover title_center_table" id="Exportar_a_Excel">
+    <thead>
+        <tr>
+            <th>RUTA-IMAGEN</th>
+            <th>ANCHO</th>
+            <th>ALTO</th>
+            <th>PESO</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        //$dato = "C:/CarlosLevano/01";
+        $dato = "D:/01_1";
+        leer_archivos_y_directorios($dato);
 
-function leer_archivos_y_directorios($ruta) {
-    if (is_dir($ruta)) {
-        // Abrimos el directorio y comprobamos que
-        if ($aux = opendir($ruta)) {
-            while (($archivo = readdir($aux)) !== false) {
-                // Si quisieramos mostrar todo el contenido del directorio pondríamos lo siguiente:
-                //echo '<br />' . $file . '<br />';
-                // Pero como lo que queremos es mostrar todos los archivos excepto "." y ".."
-                if ($archivo != "." && $archivo != "..") {
-                    $ruta_completa = $ruta . '/' . $archivo;
+        function leer_archivos_y_directorios($ruta) {
+            if (is_dir($ruta)) {
+                if ($aux = opendir($ruta)) {
+                    while (($archivo = readdir($aux)) !== false) {
+                        if ($archivo != "." && $archivo != "..") {
+                            $ruta_completa = $ruta . '/' . $archivo;
+                            if (is_dir($ruta_completa)) {
+                                //echo "<br /><strong>Directorio:</strong> " . $ruta_completa;
+                                leer_archivos_y_directorios($ruta_completa);
+                            } else {
+                                list($width, $height) = getimagesize($ruta_completa);
+                                $tamanio = filesize($ruta_completa);
+                                if ($tamanio >= 1073741824) {
+                                    $tamanio_1 = number_format($tamanio / 1073741824, 2) . ' GB';
+                                } elseif ($tamanio >= 1048576) {
+                                    $tamanio_1 = number_format($tamanio / 1048576, 2) . ' MB';
+                                } elseif ($tamanio >= 1024) {
+                                    $tamanio_1 = number_format($tamanio / 1024, 2) . ' KB';
+                                } elseif ($tamanio > 1) {
+                                    $tamanio_1 = $tamanio . ' bytes';
+                                } elseif ($tamanio == 1) {
+                                    $tamanio_1 = $tamanio . ' byte';
+                                } else {
+                                    $tamanio_1 = '0 bytes';
+                                }
 
-                    // Comprobamos si la ruta más file es un directorio (es decir, que file es
-                    // un directorio), y si lo es, decimos que es un directorio y volvemos a
-                    // llamar a la función de manera recursiva.
-                    if (is_dir($ruta_completa)) {
+                                //$cantidad = $tamanio_1;
 
-                        echo "<br /><strong>Directorio:</strong> " . $ruta_completa;
-                        leer_archivos_y_directorios($ruta_completa);
-                    } else {
+                                if ($tamanio_1 > "400") {
+                                    $back = "background: #e7443c";
+                                } else {
+                                    $back = "background: #16a085";
+                                }
 
-                        list($width, $height) = getimagesize($ruta_completa);
-                        $tamaño = filesize( $ruta_completa);
 
-                        if ($tamaño >= 1073741824) {
-                            $tamaño_1 = number_format($tamaño / 1073741824, 2) . ' GB';
-                        } elseif ($tamaño >= 1048576) {
-                            $tamaño_1 = number_format($tamaño / 1048576, 2) . ' MB';
-                        } elseif ($tamaño >= 1024) {
-                            $tamaño_1 = number_format($tamaño / 1024, 2) . ' KB';
-                        } elseif ($tamaño > 1) {
-                            $tamaño_1 = $tamaño . ' bytes';
-                        } elseif ($tamaño == 1) {
-                            $tamaño_1 = $tamaño . ' byte';
-                        } else {
-                            $tamaño_1 = '0 bytes';
+                                echo '<tr>';
+                                //echo '<td>' . $archivo . "</td>";
+                                echo '<td>' . $ruta_completa . "</td>";
+                                echo '<td>' . $width . "px</td>";
+                                echo '<td>' . $height . "px</td>";
+                                echo '<td style="' . $back . '">' . $tamanio_1 . "</td>";
+                                echo '</tr>';
+                            }
                         }
-
-                        echo '<table>';
-                        echo '<tr>';
-                        echo '<td>' . $archivo . "</td>";
-                        echo '<td>' . $width . "px</td>";
-                        echo '<td>' . $height . "px</td>";
-                        echo '<td>' . $tamaño_1 . "</td>";
-                        echo '</tr>';
-                        echo '</table>';
-
-//                        echo '<tr>';
-//                        echo '<td>' .$archivo . "</td>";
-//                        echo '</tr>';
-                        //echo '<br />' . $archivo . '<br />';
                     }
+                    closedir($aux);
+                    //echo "<strong>Fin Directorio:</strong>" . $ruta . "<br /><br />";
                 }
+            } else {
+                //echo $ruta;
+                echo "<br />No es ruta valida";
             }
-
-            closedir($aux);
-
-            // Tiene que ser ruta y no ruta_completa por la recursividad
-            //echo "<strong>Fin Directorio:</strong>" . $ruta . "<br /><br />";
         }
-    } else {
-        //echo $ruta;
-        echo "<br />No es ruta valida";
-    }
-}
+        ?>
+    </tbody>
+</table>
--- prueba.php
+++ prueba.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <!-- Enlace a Javascript -->
         <script type="text/javascript" src="assets/js/jquery-1.10.2.js"></script>
-        
+
     </head>    
     <body>
         <form action="ficheroExcel.php" method="post" target="_blank" id="FormularioExportacion">
@@ -15,7 +15,6 @@
         <table  class="table table-striped table-hover title_center_table" id="Exportar_a_Excel">
             <thead>
                 <tr>
-
                     <th>RUTA-IMAGEN</th>
                     <th>ANCHO</th>
                     <th>ALTO</th>
@@ -24,14 +23,13 @@
             </thead>
             <tbody>
                 <?php
-                //$dato = "C:/CarlosLevano/01";
+                //$dato = "D:/01";
                 $dato = "D:/01";
                 leer_archivos_y_directorios($dato);
 
                 function leer_archivos_y_directorios($ruta) {
                     if (is_dir($ruta)) {
                         if ($aux = opendir($ruta)) {
-
                             while (($archivo = readdir($aux)) !== false) {
                                 if ($archivo != "." && $archivo != "..") {
                                     $ruta_completa = $ruta . '/' . $archivo;
@@ -55,7 +53,7 @@
                                             $tamanio_1 = '0 bytes';
                                         }
 
-                                        //$cantidad = $tamanio_1;
+
 
                                         if ($tamanio_1 > "400") {
                                             $back = "background: #e7443c";
@@ -64,25 +62,26 @@
                                         }
 
 
-                                        echo '<tr>';
-                                        //echo '<td>' . $archivo . "</td>";
 
+                                        echo '<tr>';
                                         echo '<td>' . $ruta_completa . "</td>";
                                         echo '<td>' . $width . "px</td>";
                                         echo '<td>' . $height . "px</td>";
                                         echo '<td style="' . $back . '">' . $tamanio_1 . "</td>";
                                         echo '</tr>';
-
-                                        $cantidad = $ruta_completa;
-                                        $findme = 'Thumbs.db';
-                                        $pos = strpos($cantidad, $findme);
-                                        if ($pos === false) {
-                                            //echo "La cadena '$findme' no fue encontrada en la cadena '$cantidad'";
-                                        } else {
-                                            unlink($cantidad);
-//                                            echo "La cadena " . $findme . " fue encontrada en la cadena " . $cantidad . " <br>";
-//                                            echo " y existe en la posición " . $pos . " <br>";
-                                        }
+//                                        $cantidad = $ruta_completa;
+//                                        $findme = 'Thumbs.db';
+//                                        $pos = strpos($cantidad, $findme);
+//                                        if ($pos === false) {
+//                                            //echo "La cadena '$findme' no fue encontrada en la cadena '$cantidad'";
+//                                        } else {
+//                                            //unlink($cantidad);
+////                                            echo "La cadena ".$findme." fue encontrada en la cadena ".$cantidad." <br>";
+////                                            echo " y existe en la posición ".$pos." <br>";
+//                                        }
+//                                        if ($pos === false) {
+//                                            unlink($ruta_completa);
+//                                        }
                                     }
                                 }
                             }
